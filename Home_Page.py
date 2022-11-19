7988da873bc89d5b57ef87b312a003231812a79b
Augusto Gonzalez Bonorino <Augusto.GonzalezBonorino1@marist.edu>
2022-11-19 15:25:02 +0000

    First steps Environment has been set up with codespaces minimum default dependencies plus Streamlit on Python 3.10.4. A minimal sample WebApp is included.
    Just run "streamlit run Home_Page.py". Check out the docs for more details.

---
+++ Home_Page.py
@@ -0,0 +1,9 @@
+## First window to show. This is the main page of the web application.
+import streamlit as st
+import pandas as pd
+import numpy as np
+import json
+
+# Nested loop three levels deep
+
+st.title('Welcome to the main page of the web application')
