eb32096294b27199ba1f4bc5d0bc376ef2709f8f
Augusto Gonzalez Bonorino <Augusto.GonzalezBonorino1@marist.edu>
2022-11-19 12:59:51 -0800

    Added config file to configure conversant

--- pages/4_Conversational_Playground.py
+++ pages/4_Conversational_Playground.py
@@ -2,6 +2,7 @@
 import cohere as co
 from cohere.classify import Example
 from conversant import PromptChatbot
+from conversant.prompts import ChatPrompt
 
 # Sreamlit
 import streamlit as st
@@ -41,3 +42,33 @@ bot = PromptChatbot.from_persona(persona, client=co)
 first_message = st.text_input("Start a chat", key="input_prompt")
 
 st.write( bot.reply(first_message) )
+
+
+
+### Boiler plate code to train new persona
+'''
+new_persona_config = {
+    
+    "preamble": "A description of the conversation we want to account for",
+    "example_separator": "<CONVERSATION>\n",
+    "headers": 
+        {
+        "user": "who will be our audience?",
+        "bot": "who is our bot impersonating? Or, as who should the bot behave like?"
+        },
+    "examples": 
+        [
+            [
+                {
+                    "user": "First Message to start the chat",
+                    "bot": "First reply from the bot"
+                }
+            ]        
+        ]
+} # end of new_persona_config
+
+new_persona_bot = PromptChatbot(client=co, 
+                                prompt=ChatPrompt.from_dict(new_persona_config))
+
+new_persona_bot.reply("a message to the bot")
+'''
